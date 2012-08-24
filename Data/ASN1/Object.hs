5e83a5dc92a0953442d4dad893670728f4bc62c2
Vincent Hanquez <vincent@snarc.org>
2012-08-24 21:40:17 +0100

    add a class to marshall any object to/from ASN1.

---
+++ Data/ASN1/Object.hs
@@ -0,0 +1,13 @@
+module Data.ASN1.Object
+    ( ASN1Object(..)
+    ) where
+
+import Data.ASN1.Stream
+
+-- | an object that can be marshalled from and to ASN1
+class ASN1Object a where
+    -- | transform an object into an ASN1 stream.
+    toASN1   :: a      -> [ASN1]
+    -- | returns either an object along the remaining ASN1 stream,
+    -- or an error.
+    fromASN1 :: [ASN1] -> Either String (a, [ASN1])
