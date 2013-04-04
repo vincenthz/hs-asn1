3fb9b3deabce8a303a48716a597957ad0972ee39
Vincent Hanquez <vincent@snarc.org>
2013-04-04 08:24:27 +0100

    add a README

---
+++ README.hs
@@ -0,0 +1,7 @@
+ASN.1 for Haskell
+=================
+
+* asn1-types : basic types for ASN.1 representation.
+* asn1-parse : parsers for ASN.1 types.
+* asn1-encoding : BER and DER encoding decoder and encoder.
+* asn1-data: old deprecated asn1-data package containing all above.
