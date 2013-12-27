51d6be66a2ffc4612a3bc4d38109080f5ff70fa6
Vincent Hanquez <vincent@snarc.org>
2013-12-27 05:39:14 +0000

    make ParseASN1 an instance of MonadState.

--- parse/Data/ASN1/Parse.hs
+++ parse/Data/ASN1/Parse.hs
@@ -29,7 +29,7 @@ import Control.Applicative (Applicative, (<$>))
 
 -- | Parse ASN1 Monad
 newtype ParseASN1 a = P { runP :: ErrorT String (State [ASN1]) a }
-        deriving (Functor, Applicative, Monad, MonadError String)
+        deriving (Functor, Applicative, Monad, MonadError String, MonadState [ASN1])
 
 -- | run the parse monad over a stream and returns the result and the remaining ASN1 Stream.
 runParseASN1State :: ParseASN1 a -> [ASN1] -> Either String (a,[ASN1])
@@ -45,18 +45,18 @@ runParseASN1 f s = either Left (Right . fst) $ runParseASN1State f s
 -- | get next object
 getObject :: ASN1Object a => ParseASN1 a
 getObject = do
-    l <- P (lift get)
+    l <- get
     case fromASN1 l of
         Left err     -> throwError err
-        Right (a,l2) -> P (lift (put l2)) >> return a
+        Right (a,l2) -> put l2 >> return a
 
 -- | get next element from the stream
 getNext :: ParseASN1 ASN1
 getNext = do
-        list <- P (lift get)
+        list <- get
         case list of
                 []    -> throwError "empty"
-                (h:l) -> P (lift (put l)) >> return h
+                (h:l) -> put l >> return h
 
 -- | get many elements until there's nothing left
 getMany :: ParseASN1 a -> ParseASN1 [a]
@@ -69,23 +69,23 @@ getMany getOne = do
 -- | get next element from the stream maybe
 getNextMaybe :: (ASN1 -> Maybe a) -> ParseASN1 (Maybe a)
 getNextMaybe f = do
-        list <- P (lift get)
+        list <- get
         case list of
                 []    -> return Nothing
                 (h:l) -> let r = f h
                           in do case r of
-                                    Nothing -> P (lift (put list))
-                                    Just _  -> P (lift (put l))
+                                    Nothing -> put list
+                                    Just _  -> put l
                                 return r
 
 -- | get next container of specified type and return all its elements
 getNextContainer :: ASN1ConstructionType -> ParseASN1 [ASN1]
 getNextContainer ty = do
-        list <- P (lift get)
+        list <- get
         case list of
                 []                    -> throwError "empty"
                 (h:l) | h == Start ty -> do let (l1, l2) = getConstructedEnd 0 l
-                                            P (lift $ put l2) >> return l1
+                                            put l2 >> return l1
                       | otherwise     -> throwError "not an expected container"
 
 
@@ -96,11 +96,11 @@ onNextContainer ty f = getNextContainer ty >>= either throwError return . runPar
 -- | just like getNextContainer, except it doesn't throw an error if the container doesn't exists.
 getNextContainerMaybe :: ASN1ConstructionType -> ParseASN1 (Maybe [ASN1])
 getNextContainerMaybe ty = do
-        list <- P (lift get)
+        list <- get
         case list of
                 []                    -> return Nothing
                 (h:l) | h == Start ty -> do let (l1, l2) = getConstructedEnd 0 l
-                                            P (lift $ put l2) >> return (Just l1)
+                                            put l2 >> return (Just l1)
                       | otherwise     -> return Nothing
 
 -- | just like onNextContainer, except it doens't throw an error if the container doesn't exists.
@@ -113,4 +113,4 @@ onNextContainerMaybe ty f = do
 
 -- | returns if there's more elements in the stream.
 hasNext :: ParseASN1 Bool
-hasNext = not . null <$> P (lift get)
+hasNext = not . null <$> get
